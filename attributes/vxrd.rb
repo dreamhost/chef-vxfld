55fd238c23bb421528d330fb4d1aeeface15f103
Jordan Tardif <jordan@dreamhost.com>
2016-03-02 19:19:46 +0000

    Allow head_rep to be enabled via attribute
    Adds node['vxfld']['vxrd']['head_rep'] . Defaults to false

--- attributes/vxrd.rb
+++ attributes/vxrd.rb
@@ -1,3 +1,4 @@
 default['vxfld']['vxrd']['packages'] = ['vxfld']
 default['vxfld']['vxrd']['svcnode_ip'] = ''
 default['vxfld']['vxrd']['src_ip'] = node['ipaddress']
+default['vxfld']['vxrd']['head_rep'] = false
