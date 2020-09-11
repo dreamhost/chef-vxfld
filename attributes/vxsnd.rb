113083d8ae28ec77df8049459e56f01b200a74d7
David Wahlstrom <david.wahlstrom@dreamhost.com>
2020-09-11 13:17:35 -0600

    Allow configuration of max_packet_size
    The default is hard coded in the code as 1500, so I'm setting the
    attribute default to match that.
    
    Signed-off-by: David Wahlstrom <david.wahlstrom@dreamhost.com>

--- attributes/vxsnd.rb
+++ attributes/vxsnd.rb
@@ -2,3 +2,4 @@ default['vxfld']['vxsnd']['packages'] = ['vxfld']
 default['vxfld']['vxsnd']['src_ip'] = node['ipaddress']
 default['vxfld']['vxsnd']['peers'] = ''
 default['vxfld']['vxsnd']['enable_vxlan_listen'] = 'true'
+default["vxfld"]["vxsnd"]["max_packet_size"] = 1500
